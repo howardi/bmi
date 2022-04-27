98b00106c02dde63788e7c05cd0d7e38c94f5351
howardi <wakzclin@gmail.com>
2022-04-27 13:47:16 +0100

    bmi

---
+++ bmi.js
@@ -0,0 +1,24 @@
+
+var val=bmiCalculator(65,1.8);
+
+if(val < 18.5){
+  console.log("Your BMI is "+val+",so you are underweight");
+} else {
+    if ( val >= 18.5 && val <= 24.9) {
+       console.log("Your BMI is "+val+",so you have a normal weight");
+    } else {
+      if ( val > 24.9) {
+        console.log("Your BMI is "+val+",so you are overweight");
+      }
+    }
+}
+
+function bmiCalculator (weight, height) {
+     return ( Math.round((weight/(height*height))));
+}
+// question 2
+
+var age = 12;
+var accompanied = true;
+var allowed = (age >= 13) || ((age < 13) && accompanied);
+Console.log(allowed);

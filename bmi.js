50ae8d92e096a28cf8e438d202c9136b7c665059
Chita Chiamaka Sophia <65283306+chitachiamaka@users.noreply.github.com>
2022-08-11 20:38:48 +0100

    add few comment
    just to test pull request for teaching

--- bmi.js
+++ bmi.js
@@ -8,7 +8,7 @@ if(val < 18.5){
        console.log("Your BMI is "+val+",so you have a normal weight");
     } else {
       if ( val > 24.9) {
-        console.log("Your BMI is "+val+",so you are overweight");
+        console.log("Your BMI is "+val+",so you are overweight, watch your weight");
       }
     }
 }
